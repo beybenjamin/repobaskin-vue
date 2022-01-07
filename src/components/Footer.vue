3e9587f91107c5526519589e9710ffc3e7b958af
Benn <bennbey47@gmail.com>
2022-01-07 14:35:44 +0100

    Add components Header & Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,60 @@
+<template>
+    <footer class="footer">
+    <div class="footer-superieur">
+      <div class="footer-superieur-gauche">
+        <img src="imgs/Logo_BaskIn_Grand_Blanc.png">
+        <img src="imgs/icons8-f-de-facebook-50.png">
+        <img src="imgs/icons8-instagram-64.png">
+        <img src="imgs/icons8-tic-tac-50.png">
+      </div>
+      <div class="footer-superieur-droit">
+        <div class="menu-footer-1">
+          <h5>Votre compte</h5>
+          <ul>
+            <li><a href="Profil_monProfil.html">Mon profil</a></li>
+            <li><a href="creation.html">Mes créations</a></li>
+          </ul>
+        </div>
+        <div class="menu-footer-2">
+          <h5>Baskin</h5>
+          <ul>
+            <li><a href="marque.html">La marque</a></li>
+            <li><a href="personalisation.html">Ma paire</a></li>
+            <li><a href="galerie.html">La galerie</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </div>
+        <div class="mune-footer-3">
+          <h5>Informations</h5>
+          <ul>
+            <li><a href="cgu.html">Condition général d'utilisation et de vente</a></li>
+            <li><a href="livraison.html">Livraison</a></li>
+            <li><a href="mention.html">Mentions légales</a></li>
+            <li><a href="plan.html">Plan HTML</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-inferieur">
+      <div class="footer-inferieur-gauche">
+        <!--LAISSER VIDE !-->
+      </div>
+      <div class="footer-inferieur-milieu">
+        <p>Copyright © 2021 BASKIN SARL</p>
+      </div>
+      <div class="footer-inferieur-droit">
+        <img src="imgs/icons8-amex-100.png">
+        <img src="imgs/icons8-mastercard-100.png">
+        <img src="imgs/icons8-paypal-an-online-payments-system-operating-worldwide-48.png">
+        <img src="imgs/icons8-visa-60.png">
+      </div>
+    </div>
+  </footer>
+</template>
+
+
+<style lang="scss" scoped>
+
+// style du footer
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="header">
+    <div class="e-logo">
+      <a href="#"><img class="e-logo__img" src="/src/imgs/Logo.png" /></a>
+    </div>
+    <div class="e-menu">
+      <ul class="e-menu__ul">
+        <li class="e-menu__li">
+          <a class="e-decoration" href="marque.html">La marque</a>
+        </li>
+        <li class="e-menu__li">
+          <a class="e-decoration" href="galerie.html">La galerie</a>
+        </li>
+        <li class="e-menu__li">
+          <a class="e-decoration" href="contact.html">Contact</a>
+        </li>
+        <li class="e-menu__li">
+          <a class="e-decoration" href="personalisation.html">Ma paire</a>
+        </li>
+        <li class="e-menu__li">
+          <a href="#"><img src="/src/imgs/icon_profil.png" alt="" /></a>
+        </li>
+        <li class="e-menu__li">
+          <a href="#"><img src="/src/imgs/icon_cart.png" alt="" /></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#header{
+    background-color: $colordarkgreen;
+    max-height: 90px;
+    max-width: 1200px;
+    border-bottom-left-radius: 30px ;
+    border-bottom-right-radius: 30px ;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: auto;
+    box-shadow: 1px 1px 5px 2px $colorshadow;
+
+}
+
+.e-logo{
+    width: auto;
+    display: flex;
+    margin: 0px 35px;
+}
+
+.e-logo__img{
+    max-width: 200px;
+    margin: 10px;
+}
+
+
+.e-decoration{
+    font-size: 20px;
+    text-decoration: none;
+    color: $colorwhite;
+    font-family: $fontTexte;
+    
+}
+
+.e-menu__ul{
+    display: flex;
+    flex-direction: row;
+}
+.e-menu__li{
+    display: flex ;
+    flex-direction: row;
+    margin-right: 35px;
+    text-decoration: none;
+    align-items: center;
+    
+}
+</style>
