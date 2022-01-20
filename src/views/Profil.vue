2136b27e81d06457b54c6e940bdf8539bb70c142
Benn <bennbey47@gmail.com>
2022-01-20 10:02:51 +0100

    Profil.vue

---
+++ src/views/Profil.vue
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="header">
+        <div class="e-logo">
+         <a href="index.html"><img class="e-logo__img" src="/src/imgs/Logo.png"></a>
+        </div>
+        <div class="e-menu">
+          <ul class="e-menu__ul">
+            <li class="e-menu__li"><a  class="e-decoration" href="marque.html">La marque</a></li>
+            <li class="e-menu__li"><a class="e-decoration" href="galerie.html">La galerie</a></li>
+            <li class="e-menu__li"><a class="e-decoration" href="contact.html">Contact</a></li>
+            <li class="e-menu__li"><a class="e-decoration" href="personalisation.html">Ma paire</a></li>
+            <li class="e-menu__li">
+              <a href="#"><img src="/src/imgs/icon_profil.png" alt=""></a>
+            </li>
+            <li class="e-menu__li">
+              <a href="#"><img src="/src/imgs/icon_cart.png" alt=""></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    <div class="c-menu">
+        <ul class="e-menu">
+            <li class="e-menu_li">Mon Profil</li>
+            <li class="e-menu_li">Mot de Passe</li>
+            <li class="e-menu_li">Adresse de livraison</li>
+            <li class="e-menu_li">Mes précommandes</li>
+            <li class="e-menu_li">Mes créations</li>
+        </ul>
+    </div>
+    <div>
+        <div class="e-titre">
+            <p>Bienvenue à toi Théo,</p>
+            <p>Nous sommes ravis de te retrouver</p>
+        </div>
+        <div class="c-monProfil">
+            <div class="c-contenu">
+                <div class="c-introduction">
+                    <h3 class="c-introduction__h3">Mon profil</h3>
+                    <p>Ici tu vas pouvoir modifier tes informations personnels. </p>
+                    <p>Nous te promettons de les garder rien que pour nous.</p>
+                </div>
+                <div class="c-photo">
+                    <div class="e-photo">
+                        <img class="e-photo__img" src="#" alt="Photo d'un jeune homme">
+                    </div>
+                    <button class="e-fullbutton">Modifier la photo</button>
+                    <button class="e-emptybutton">Supprimer la photo</button>
+                </div>
+                <div class="c-formulaire">
+                    <form class="e-formulaire" method="post">
+                        <ul>
+                            <li class="e-formlist">
+                                <p class="e-formlist__titre">Prénom</p>
+                                <input class="e-formlist__input" type="text" name="Prénom">
+                            </li>
+                            <li class="e-formlist">
+                                <p class="e-formlist__titre">Nom</p>
+                                <input  class="e-formlist__input" type="text" name="Nom">
+                            </li>
+                            <li class="e-formlist">
+                                <p class="e-formlist__titre">Pseudo</p>
+                                <input class="e-formlist__input" type="text" name="Pseudo">
+                            </li>
+                            <li class="e-formlist">
+                                <p class="e-formlist__titre">E-mail</p>
+                                <input class="e-formlist__input" type="text" name="E-mail">
+                            </li>
+                            <li class="e-formlist">
+                                <p class="e-formlist__titre">Téléphone</p>
+                                <input class="e-formlist__input" type="text" name="Téléphone">
+                            </li>
+                        </ul>
+                    </form>
+                    <button class="e-emptybutton">Sauvegarder</button>
+                </div>
+            </div>
+        </div>
+        <div class="c-motDePasse">
+            <div class="c-contenu">
+                <div class="c-introduction">
+                    <h3 class="c-introduction_h3">Mot de Passe</h3>
+                    <p>Ici tu vas pouvoir modifier ton mot de passe </p>
+                    <p>Ne l'oublie surtout pas !</p>
+                </div>
+                <div class="c-formulaire">
+                    <form method="post">
+                        <ul>
+                            <li class="e-formlist">
+                                <p class="e-formlist__titre">Mot de passe actuel</p>
+                                <input class="e-formlist__input" type="text" name="MDPactuel">
+                            </li>
+                            <li class="e-formlist">
+                                <p class="e-formlist__titre">Nouveau mot de passe</p>
+                                <input class="e-formlist__input" type="text" name="NvMDP">
+                            </li>
+                            <div class="e-formlist">
+                                <p class="e-formlist__titre">Retaper le mot de passe</p>
+                                <input class="e-formlist__input" type="text" name="RetaperMDP">
+                            </div>
+                        </ul>
+                    </form>
+                    <button class="e-emptybutton">Sauvegarder</button>
+                </div>
+            </div>
+        </div>
+        <div class="c-adresseDeLivraison">
+            <div class="c-contenu">
+                <div class="c-introduction">
+                    <h3 class="c-introduction_h3">Adresse de livraison</h3>
+                    <p>Ici tu vas pouvoir modifier tes adresses de livraisons </p>
+                </div>
+                <div class="">
+                    <button class="e-emptybutton">
+                        Ajouter une adresse
+                    </button>
+                    <div class="e-rubrique">
+                        <div class="e-rubrique__haut">
+                            <div>
+                                <p>Théo Legrand</p>
+                            </div>
+                            <div>
+                                <p>Adresse par défaut</p>
+                            </div>
+                        </div>
+                        <div class="e-rubrique__bas">
+                            <p>36 rue de la Liberté</p><br>
+                            <p>Montbéliard, Franche-Comté, 25200</p><br>
+                            <p>France</p><br>
+                        </div>
+                        <button class="e-emptybutton">Modifier</button>
+                        <button class="e-emptybutton">Effacer</button>
+                    </div>
+                    <div class="e-rubrique">
+                        <div class="e-rubrique__haut">
+                            <div>
+                                <p>Théo Legrand</p>
+                            </div>
+                        </div>
+                        <div class="e-rubrique__bas">
+                            <p>86 rue de la Concorde</p><br>
+                            <p>Lens, Haut de France, 62300</p><br>
+                            <p>France</p><br>
+                        </div>
+                        <button class="e-emptybutton">Modifier</button>
+                        <button class="e-emptybutton">Effacer</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="c-ajouterUneAdresse">
+            <div class="c-contenu">
+                <div class="c-introduction">
+                    <h3 class="c-introduction_h3">Ajouter une adresse de livraison</h3>
+                    <p>Ici tu vas pouvoir ajouetr une nouvelle adresse de livraison</p>
+                </div>
+                <button class="e-emptybutton">Utiliser l'emplacement actuel</button>
+                <div class="c-formulaire">
+                    <form method="post">
+                        <ul>
+                            <li class="e-formlist__adresse">
+                                <p class="e-formlist__titre">Adresse</p>
+                                <input class="e-formlist__input" type="text" name="Adresse">
+                                <input class="e-formlist__input" type="text" name="Adresse">
+                            </li>
+                            <li class="e-formlist">
+                                <p class="e-formlist__titre">Code postal</p>
+                                <input class="e-formlist__input" type="text" name="Code postal">
+                            </li>
+                            <li class="e-formlist">
+                                <p class="e-formlist__titre">Ville</p>
+                                <input class="e-formlist__input" type="text" name="Ville">
+                            </li>
+                        </ul>
+                    </form>
+                    <input type="checkbox" id="e-chexbox" name="Défaut">
+                    <label for="e-chexbox">Faire de cette adresse mon adresse par défaut</label><br>
+                    <button class="e-emptybutton">Ajouter</button>
+                </div>
+            </div>
+        </div>
+        <div class="c-mesPrecommandes">
+            <div class="c-contenu">
+                <div class="c-introduction">
+                    <h3 class="c-introduction_h3">Mes précommandes</h3>
+                    <p>Ici tu vas pouvoir retrouver toutes tes précommandes</p>
+                </div>
+                <div class="c-précommandes">
+                    <div class="e-chaussures">
+                        <img src="#" alt="Photo d'une basquette">
+                    </div>
+                    <div class="e-informations">
+                        <p><strong>Nom de la basket</strong></p>
+                        <p>La Bask'It</p>
+                        <p><strong>Taille de la basket</strong></p>
+                        <p>40</p>
+                        <p><strong>Date de livraison estimé</strong></p>
+                        <p>janvier 2022</p>
+                    </div>
+                    <button class="e-emptybutton">Télécharger la facture</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-inferieur">
+        <div class="footer-inferieur-gauche">
+            <!--LAISSER VIDE !-->
+        </div>
+        <div class="footer-inferieur-milieu">
+            <p>Copyright © 2021 BASKIN SARL</p>
+        </div>
+        <div class="footer-inferieur-droit">
+            <img src="imgs/icons8-amex-100.png">
+            <img src="imgs/icons8-mastercard-100.png">
+            <img src="imgs/icons8-paypal-an-online-payments-system-operating-worldwide-48.png">
+            <img src="imgs/icons8-visa-60.png">
+        </div>
+    </div>
+</body>
+
+</html>
