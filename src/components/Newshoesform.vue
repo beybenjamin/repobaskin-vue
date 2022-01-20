7b1fb4267f9bce13b0739f4015da20e1c813aa91
Benn <bennbey47@gmail.com>
2022-01-20 15:40:22 +0100

    Token + sauvegarde shoes

--- src/components/Newshoesform.vue
+++ src/components/Newshoesform.vue
@@ -1,40 +1,61 @@
 <template>
-
-<div>
-    <input type="text" v-model="shoeName" placeholder="nom de la chaussure">
+  <div>
+    <input type="text" v-model="shoeName" placeholder="nom de la chaussure" />
     <div class="canvas">
-        <p>Test de dom to image</p>
-        <img src="@/assets/logo.png" alt="">
+      <p>Test de dom to image</p>
+      <img src="@/assets/logo.png" alt="" />
     </div>
     <button @click="screenShot">Sauvegarde</button>
-</div>  
+  </div>
 </template>
+
+
 <script>
-/*import axios from "axios";*/
-import domtoimage from "dom-to-image-more"
+import domtoimage from "dom-to-image-more";
+import axios from "axios";
 
-export default{
-    data (){
-        return{
-            shoeName: ''
-        }
+export default {
+  data() {
+    return {
+      shoeName: "",
+    };
+  },
+  methods: {
+    screenShot() {
+      domtoimage
+        .toJpeg(document.querySelector(".canvas"), { quality: 0.95 })
+        .then( (dataUrl) => {
+          var link = document.createElement("a");
+          link.download = "my-image-name.jpeg";
+          link.href = dataUrl;
+          link.click();
+
+          this.submitShoe();
+        });
     },
-    methods: {
-    screenShot(){
 
-        domtoimage.toJpeg(document.querySelector('.canvas'), { quality: 0.95 })
-    .then(function (dataUrl) {
-        var link = document.createElement('a');
-        link.download = 'my-image-name.jpeg';
-        link.href = dataUrl;
-        link.click();
-    });
-    }
-}
-}
-
-
+    submitShoe() {
+      axios
+        .post(
+          "https://sitebaskin.benjaminbey.fr/wp-json/wp/V2/chaussures",
+          {
+            status: "publish",
+            title: this.$store.state.user.displayName,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${this.$store.state.user.authToken}`,
+            },
+          }
+        )
+        .then((response) => {
+          if (response.status === 200) {
+            console.log("La chaussure est enregistrée");
+          }
+        });
+    },
+  },
+};
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,104 +1,104 @@
 <template>
-    <div>
-    <newshoesform/> 
-        <form @submit="submit">
-            <div>
-                <label for="username">Nom utilisateur</label>
-                <input id="username" type="text" v-model="form.username">
-            </div>
-            <div>
-                <label for="password">Mot de passe</label>
-                <input type="password" id="password" v-model="form.password">
-            </div>
- 
-            <input type="submit" value="submit">
-        </form>
- 
-        <div v-if="success">
-            <p style="color: green;">Votre inscription est réussi !</p>
-        </div>
- 
-        <div v-if="error">
-            <p style="color: red;">{{ errorMessage }}</p>
-        </div>
+  <div>
+    <Newshoesform />
+
+    <form @submit="submit">
+      <div>
+        <label for="username">Nom utilisateur</label>
+        <input id="username" type="text" v-model="form.username" />
+      </div>
+      <div>
+        <label for="password">Mot de passe</label>
+        <input type="password" id="password" v-model="form.password" />
+      </div>
+
+      <input type="submit" value="submit" />
+    </form>
+
+    <div v-if="success">
+      <p style="color: green">Votre inscription est réussi !</p>
     </div>
+
+    <div v-if="error">
+      <p style="color: red">{{ errorMessage }}</p>
+    </div>
+  </div>
 </template>
- 
+
 <script>
-import Newshoesform from "@/components/Newshoesform"
-import axios from "axios"
+import Newshoesform from "@/components/Newshoesform";
+import axios from "axios";
 export default {
-    components: {
-        Newshoesform
+  components: {
+    Newshoesform,
+  },
+  data() {
+    return {
+      form: {
+        username: null,
+        password: null,
+      },
+      success: false,
+      error: false,
+      errorMessage: null,
+    };
+  },
+  computed: {
+    user() {
+      return this.$store.state.user;
     },
-    data() {
-        return {
-            form: {
-                username: null,
-                password: null,
-            },
-            success: false,
-            error: false,
-            errorMessage: null
-        }
-    },
-    computed:{
-        user(){
-            return this.$store.state.user
-        },
-    },
-    
-    mounted() {
-        
-    },
-    methods: {
-        submit(event) {
-            event.preventDefault()
- 
-            axios.post('https://sitebaskin.benjaminbey.fr/wp-json/jwt-auth/v1/token', { 
-                username: this.form.username,
-                password: this.form.password
-            }).then(response => {
-                if (response.status === 200) {
-                    this.success = true
+  },
 
-                  this.$store.commit( 'setUser', {
-                        username: response.data.data.displayName ,
-                        email: response.data.data.email ,
-                        authToken: response.data.data.token
-                    })
+  mounted() {},
+  methods: {
+    submit(event) {
+      event.preventDefault();
 
-                    this.error = false
-                }
-            }).catch(error => {
-                console.log('Error LOG : ', error.response)
-                this.errorMessage = error.response.data.message
-                this.error = true
-                this.success = false
-            })
-        }
- 
- 
-    }
-}
+      axios
+        .post("https://sitebaskin.benjaminbey.fr/wp-json/jwt-auth/v1/token", {
+          username: this.form.username,
+          password: this.form.password,
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            this.success = true;
+
+            this.$store.commit("setUser", {
+              username: response.data.data.displayName,
+              email: response.data.data.email,
+              authToken: response.data.data.token,
+            });
+
+            this.error = false;
+          }
+        })
+        .catch((error) => {
+          console.log("Error LOG : ", error.response);
+          this.errorMessage = error.response.data.message;
+          this.error = true;
+          this.success = false;
+        });
+    },
+  },
+};
 </script>
- 
+
 <style lang="scss">
 form {
-    text-align: left;
-    width: 980px;
-    margin: auto;
- 
-    div {
-        display: flex;
- 
-        label {
-            width: 200px;
-        }
- 
-        input {
-            width: 100%;
-        }
+  text-align: left;
+  width: 980px;
+  margin: auto;
+
+  div {
+    display: flex;
+
+    label {
+      width: 200px;
     }
+
+    input {
+      width: 100%;
+    }
+  }
 }
 </style>
